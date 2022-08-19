c3d6f8a8767fa1277f9617b25b33197b44af44c1
Fevronia Gjini <fevronigjini1@gmail.com>
2022-08-19 02:18:43 +0200

    working with X and O

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,40 @@ import { Board } from './components/Board'
 import { Square } from './components/Square'
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
+  const [board, setBoard] = useState(Array(9).fill(""))
+  function computerMove() {
+    const copyBoard = structuredClone(board)
+    const selectedByComputer = Math.floor(Math.random() * 9);
+    copyBoard[selectedByComputer] = "X"
+    setBoard(copyBoard)
+  }
 
+  function playerMove(index: number) {
+    const copyBoard = structuredClone(board)
+    if (copyBoard[index] === "") {
+      copyBoard[index] = 'O'
+    }
+    setBoard(copyBoard)
+    computerMove()
 
+  }
   return (
     <div className="App">
 
       <div className="container">
         <h1>Tic-Tac-Toe by Eva</h1>
-        <div className="play-area">
+        <div
+          className="play-area">
+
           {board.map((block, i) => (
-            <div className={block === 'X' || block === 'O'
-              ? "block occupied" : "block"
-            }></div>
+            <div
+              key={i}
+              className={block === 'X' || block === 'O'
+                ? "block occupied" : "block"
+              }
+              onClick={() => playerMove(i)}>
+              {block}
+            </div>
           ))}
         </div>
 
